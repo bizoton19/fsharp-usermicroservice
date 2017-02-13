5e515ab0549de9e0620a24222007880c704516cb
Alexandre Salomon <bizoton19@gmail.com>
2017-02-12 23:38:31 -0500

    new proj

---
+++ UserMicroServce/UserServiceHttpServive/UserRepository.fs
@@ -0,0 +1,27 @@
+﻿namespace Generic.SuaveRestAPI.Repository
+open System.Collections.Generic
+type User = {
+   
+    UserName: string
+    Email: string
+    FirstName: string
+    LastName: string
+}
+module UserRepository=
+   
+    let private userStore = new Dictionary<string,User>()
+    let getUsers ()=
+        userStore.Values |> Seq.map(fun u-> u)
+
+    let createUser user = 
+        let newUser = {
+            UserName = user.UserName
+            Email = user.Email
+            FirstName = user.FirstName
+            LastName = user.LastName
+        }
+        userStore.Add(newUser.UserName,newUser)
+        newUser
+        
+        
+
---
+++ UserMicroServce/UserServiceHttpServive/UserService.fs
@@ -0,0 +1,50 @@
+﻿namespace Generic.SuaveRestAPi.Rest
+open Suave
+open Newtonsoft.Json
+open Newtonsoft.Json.Serialization
+open Suave.Successful
+open Suave.Operators
+open Suave.Http
+
+[<AutoOpen>]
+module RestService=
+    open Suave.RequestErrors
+    open Suave.Filters
+    
+    type RestResource<'a>={
+      GetAll:unit-> 'a seq
+      //GetByUserName: unit-> 'a seq
+      Create: 'a->'a
+    }
+      //string -> RestResource<'a> -> Webpart
+   
+    let JSON resource = 
+        let newtonJsonSerializerSettings = new JsonSerializerSettings()
+        newtonJsonSerializerSettings.ContractResolver <- new CamelCasePropertyNamesContractResolver()
+
+        JsonConvert.SerializeObject(resource,newtonJsonSerializerSettings)
+        |> OK
+        >=> Writers.setMimeType "application/json; charset=utf-8"
+
+    let fromJson<'a> json = 
+        JsonConvert.DeserializeObject(json, typeof<'a>):?> 'a //que?
+
+    let getResourceFromRequest<'a> (request:HttpRequest)=
+        let getString rawRequestValue = 
+            System.Text.Encoding.UTF8.GetString(rawRequestValue)
+        request.rawForm |> getString |> fromJson<'a>
+
+    let rest resourceName resource =
+        let resourcePath = "/" + resourceName
+        let getAll = warbler(fun _-> resource.GetAll ()|> JSON)
+       
+        path resourcePath >=> choose[
+            GET >=> getAll
+            POST >=> request  ( getResourceFromRequest >> resource.Create>>JSON)
+            ]
+
+    
+   
+
+   
+    
